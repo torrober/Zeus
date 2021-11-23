775c941fbfd14ff6143f101b90b13db06ea18c19
Roberto M. Rocha <robemario124@gmail.com>
2021-11-23 15:56:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 import json
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, logger=True)
+socketio = SocketIO(app,logger=True, engineio_logger=True)
 CORS(app)
 socketio.init_app(app, cors_allowed_origins="*")
 users = []
